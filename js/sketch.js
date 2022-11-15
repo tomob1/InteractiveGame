6cea9972fc8583e5b498252a265d41cf06d87d20
Tom O'Brien <116882874+tomob1@users.noreply.github.com>
2022-11-15 10:16:01 +0000

    Add files via upload
    Added different mouse functions + colours

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,45 @@
+var widthRect = 70;
+var heightRect = 70;
+
 function setup() {
     var canvas = createCanvas(800, 800);
     canvas.parent("p5container");
+    background(220);
 }
 
 function draw() {
-    background(220);
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+
+
+}
+
+function mousePressed() {
+    if (mouseButton == LEFT) {
+        fill(255, 255, 255);
+        rectMode(CENTER)
+        rect(mouseX, mouseY, widthRect, heightRect);
+    }
+
+    if (mouseButton == RIGHT) {
+        fill(255, 255, 255);
+        ellipseMode(CENTER)
+        ellipse(mouseX, mouseY, widthRect, heightRect);
+    }
+
+}
+
+function keyPressed() {
+    if (keyCode == ENTER){
+        background(0)
+}
+if (keyCode == UP_ARROW) {
+    rect(mouseX, mouseY, widthRect+20, widthRect+40)
+    
+    }
+
+if (keyCode == DOWN_ARROW) {
+    fill(random(0,255), random(0,255), random(0,255))
+    triangle(mouseX,mouseY,mouseX=50,mouseY, mouseX=25, mouseY-50)
+}
+
 }
